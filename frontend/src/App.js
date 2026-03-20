cd9d2b98b39b3b23a7b0c4e91c68351cec18f481
Jananga07 <janangachandima07@gmail.com>
2026-03-21 01:08:25 +0530

    loging setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import SocietyPage from './Components/Society/SocietyPage';
 import ModuleDetailsPage from './Components/ModulePage/ModuleDetailsPage';
 import StudentSupport from './Components/StudentSupport/StudentSupport';
 import ConsultantBooking from './Components/ConsultantBooking/ConsultantBooking';
+import SocietyManagerProfile from './Components/Society/SocietyManagerProfile';
 
 
 // Resources pages
@@ -33,6 +34,9 @@ function App() {
           <Route path="/userRegister"  element={<UserRegister />} />
           <Route path="/login"         element={<Login />} />
           <Route path="/studentprofile/:id" element={<StudentProfile />} />
+          <Route path="/societymanagerprofile/:id" element={<SocietyManagerProfile />} />
+          {/* Compatibility route (case-sensitive match in some setups) */}
+          <Route path="/SocietyManagerProfile/:id" element={<SocietyManagerProfile />} />
           <Route path="/admin"         element={<AdminDashboard />} />
           <Route path="/adquiz"        element={<AddQuiz />} />
           <Route path="/student-quiz/:moduleId" element={<StudentQuiz />} />
@@ -40,6 +44,7 @@ function App() {
           <Route path="/societypage"   element={<SocietyPage />} />
           <Route path="/modules/:id"   element={<ModuleDetailsPage />} />
           <Route path="/studentsupport" element={<StudentSupport />} />
+          
 
           <Route path="/modulepage" element={<ModulePage/>}/>
           <Route path="/societypage" element={<SocietyPage/>}/>
