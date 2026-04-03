243ff953e83ad9d844c7deada1e5ad03f372969d
Sujan Sandeepa <s.sandeepatech@gmail.com>
2026-04-04 01:47:37 +0530

    Update membershipRoutes.js

--- backend/Routes/membershipRoutes.js
+++ backend/Routes/membershipRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/apply", MembershipController.applyMembership);
 router.get("/manager/:managerId", MembershipController.getManagerRequests);
+router.get("/student/:userId", MembershipController.getStudentRequests);
 router.patch("/:id", MembershipController.updateMembershipStatus);
 
 module.exports = router;
