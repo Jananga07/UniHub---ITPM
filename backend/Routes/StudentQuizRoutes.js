8ed765ed3754cd705cc94831ce8045f30385d63f
Sujan Sandeepa <s.sandeepatech@gmail.com>
2026-04-26 01:16:52 +0530

    Update StudentQuizRoutes.js

--- backend/Routes/StudentQuizRoutes.js
+++ backend/Routes/StudentQuizRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { submitStudentQuiz, getLeaderboard, getStudentQuizHistory, getModuleAttemptCounts } = require("../Controllers/StudentQuizController");
+const { submitStudentQuiz, getLeaderboard, getStudentQuizHistory } = require("../Controllers/StudentQuizController");
 
 router.post("/submit", submitStudentQuiz);
 router.get("/leaderboard", getLeaderboard);
 router.get("/history/:studentId", getStudentQuizHistory);
-router.get("/module-attempts", getModuleAttemptCounts);
 
 module.exports = router;
